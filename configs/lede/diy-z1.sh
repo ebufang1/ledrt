100ad2ac60c0b44398799863ded26180abcd198c
fbufang6 <fbufang6@gmail.com>
2025-06-20 08:01:34 +0800

    Update diy-z1.sh

--- configs/lede/diy-z1.sh
+++ configs/lede/diy-z1.sh
@@ -17,6 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
 
 sed -i "48a\\
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config\n\
