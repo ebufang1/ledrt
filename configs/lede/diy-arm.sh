9986065c0742b6657dab86f89cbda210ec004bd8
fbufang6 <fbufang6@gmail.com>
2025-08-10 10:45:37 +0800

    Update diy-arm.sh

--- configs/lede/diy-arm.sh
+++ configs/lede/diy-arm.sh
@@ -39,8 +39,8 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang -b 1.23 feeds/packages/lang/golang
 git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-sed -i 's|^PKG_VERSION.*|PKG_VERSION:=25.2.21|' feeds/small/xray-core/Makefile
-sed -i 's|^PKG_HASH.*|PKG_HASH:=a565db518d2da12fabb74e123d9bf2bdbc34420b81373938f8fcbc7004fda3ba|' feeds/small/xray-core/Makefile
+#sed -i 's|^PKG_VERSION.*|PKG_VERSION:=25.2.21|' feeds/small/xray-core/Makefile
+#sed -i 's|^PKG_HASH.*|PKG_HASH:=a565db518d2da12fabb74e123d9bf2bdbc34420b81373938f8fcbc7004fda3ba|' feeds/small/xray-core/Makefile
 
 # Delete mosdns
 #rm -rf feeds/packages/net/mosdns
